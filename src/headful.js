0db8197486a8d9cd2d4a5589776fa671efdefaf9
troxler <2767948+troxler@users.noreply.github.com>
2018-01-09 14:58:46 +0100

    Throw error when noProp() is being called without props argument

--- src/headful.js
+++ src/headful.js
@@ -47,6 +47,9 @@ headful.props = propertySetters;
  * Tests whether the given `props` object contains a property with the name of `propNameOrFunction`.
  */
 function noProp(props, propNameOrFunction) {
+    if (!props) {
+        throw new Error('Headful: You must pass all declared props when you use headful.props.x() calls.');
+    }
     const propName = typeof propNameOrFunction === 'function' ? propNameOrFunction.name : propNameOrFunction;
     return !props.hasOwnProperty(propName);
 }
